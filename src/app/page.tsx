de6a3640b99875529d9d04e383c93b71f4ba7c8c
CodesByAbdul <adigunabdulrahman3@gmail.com>
2025-07-27 19:13:20 +0100

    header removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
   return (
     <ThemeProvider>
       <main className="min-h-screen bg-background">
-        <Header />
         <Hero />
         <About />
         <Skills />
